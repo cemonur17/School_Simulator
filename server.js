814af7e49af3129014e5ca86d2fdb4750ee5dea3
cemonur17 <cemonur@gmail.com>
2026-06-10 06:11:09 +0300

    server.js updated

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('public'));
+// 1. BU SATIR ÇOK ÖNEMLİ: index.html dosyasını ana sayfa yapar
+app.use(express.static(__dirname)); 
+
+// 2. Eğer ana dizinde index.html varsa, onu otomatik gönderir
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
   console.log('Bir öğrenci sınıfa girdi.');
-
-  // Mesajlaşma
-  socket.on('mesajGonder', (data) => io.emit('mesajGeldi', data));
   
-  // Takas ve Hediye
+  socket.on('mesajGonder', (data) => io.emit('mesajGeldi', data));
   socket.on('takasTeklifi', (data) => io.emit('takasGeldi', data));
 });
 
-http.listen(3000, () => console.log('Sınıf kapıları açıldı!'));
+// Port 3000 değil, Render'ın verdiği portu kullanmalıyız
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Sınıf kapıları ${PORT} portunda açıldı!`));
